7b63a9690aa3690f51473174336deb286a4805f3
elvxk <pradiptaramadan9c@gmail.com>
2024-11-20 15:42:03 +0700

    feat(account): Add account detail

--- src/components/BtnSignOut.jsx
+++ src/components/BtnSignOut.jsx
@@ -1,10 +1,17 @@
 "use client";
 import { useClerk } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { IoLogOut } from "react-icons/io5";
 
 export const BtnSignOut = () => {
   const { signOut } = useClerk();
 
   return (
-    <button onClick={() => signOut({ redirectUrl: "/" })}>Sign out</button>
+    <Button
+      onClick={() => signOut({ redirectUrl: "/" })}
+      className="flex items-center gap-2 bg-white"
+    >
+      <IoLogOut /> Sign out
+    </Button>
   );
 };
